0e5a3512bbadf030d09d7888d8c975c93fe51b7a
Guillermo Paredes Macho <2c62b044cbc469601ae3bdade3f7c7b2db07f3ab>
2020-09-27 18:09:15 +0100

    iniciando logica operaciones

--- calculadoraGPM/ViewController.swift
+++ calculadoraGPM/ViewController.swift
@@ -27,31 +27,76 @@ class ViewController: UIViewController,CalculadoraProtocol {
         case "=":
             break;
         default:
+            inNumsArray.append((button as NSString).floatValue);
+            viewUpdate();
             break;
             
         }
             
     }
-    
     //convertir numeros sueltos a un solo valor y añadir el operador
     func addOperador(opr: String){
-        operadArray[operadArray.count] = opr;
-        numArray[numArray.count] = numProcess();
+        var oprd = "";
+        switch opr{
+        case "mul":
+            oprd = "*";
+            break;
+        case "div":
+            oprd = "/";
+            break;
+        case "rest":
+            oprd = "%";
+            break;
+        default:
+            oprd = opr;
+            break;
+        }
+        operadArray.append(oprd);
+        numArray.append(numProcess(res: true));
+        viewUpdate();
+    }
+    
+    //num1 opr1 num2 = res1 // res1 opr2 num3 = res2 ... // resx oprx+1 numin = res final
+    func operar (){
         
     }
     
     //procesar el nuevo numero que se encuentra separado een numeros diferentes
-    func numProcess() -> Float{
+    //nums:1,2,3,4 -> num = 1*1000+2*100+3*10+4*1
+    func numProcess(res:Bool) -> Float{
         var newNum: Float = 0;
         if(inNumsArray.count > 0){
-            //proceso de numeros a numero solitario
-            inNumsArray = [Float] ();
+            
+            var x = inNumsArray.count
+            repeat{
+                var y = inNumsArray.count - x;
+                x = x - 1;
+                var numpos: Float = 1;
+       	         while (y > 0){
+                    numpos = numpos * 10;
+                    y = y-1;
+                }
+                newNum = newNum + inNumsArray[x] * numpos;
+            }while x > 0
+            
+            if(res){
+                inNumsArray = [Float] ();
+            }
+            
         }
         return newNum;
     }
     
+    //num0,oper0,num1,oper1... numx,operx,newnum
     func viewUpdate(){
-        
+        var x = 0;
+        var text:String = "";
+        while x < numArray.count{
+            text = text + numArray[x].description + " " + operadArray[x].description + " ";
+            x = x + 1;
+        }
+        text = text + numProcess(res: false).description;
+        pantallaTw.text = text;
     }
     
     func suma(num1: Int, num2: Int)  -> Int {
